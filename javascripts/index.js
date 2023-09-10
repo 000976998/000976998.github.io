ec6b6f1e5268e2392dcfe4a3fc2803619b5735db
Mostafa ANSARI <mostafa.ansari@student.tafesa.edu.au>
2023-09-10 16:28:39 +0930

    add initial files.

---
+++ javascripts/index.js
@@ -0,0 +1,18 @@
+// Page : script file for index page.
+// Author : Mostafa ANSARI.
+// Date : Sep - 2023.
+
+// preload scripts.
+const display =()=>{
+    titleContext();
+}
+
+// title
+const titleContext =()=>{
+    let title;
+    let titleText;
+
+    title = document.getElementById('title');
+    titleText = 'HOME';
+    title.innerText = titleText;
+} 
